36aea3c4711fa9f34c30e190d570a6985c24495c
caesarhu <hupeishun@gmail.com>
2020-05-29 16:41:50 +0800

    move [funcool/struct 1.4.0] to dev/dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject taiwan-id "0.4.0"
+(defproject taiwan-id "0.4.1"
   :description "clojure台灣身分證字號檢核程式庫，包含新版及舊版外來人口統一證號檢核"
   :url "https://github.com/caesarhu/taiwan-id.git"
   :license {:name "Eclipse Public License"
@@ -11,8 +11,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [duct/core "0.8.0"]
                  [duct/module.logging "0.5.0"]
-                 [org.clojure/tools.reader "1.3.2"]
-                 [funcool/struct "1.4.0"]]
+                 [org.clojure/tools.reader "1.3.2"]]
   :plugins [[duct/lein-duct "0.12.1"]]
   :main ^:skip-aot taiwan-id.main
   :resource-paths ["resources" "target/resources"]
@@ -31,4 +30,5 @@
                                    [hawk "0.2.11"]
                                    [orchestra "2019.02.06-1"]
                                    [expound "0.8.4"]
+                                   [funcool/struct "1.4.0"]
                                    [eftest "0.5.9"]]}})
