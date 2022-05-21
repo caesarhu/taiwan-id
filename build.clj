82d417f821c37df4532bcea972d61a54cf6f2e38
caesarhu <hupeishun@gmail.com>
2022-05-21 08:09:13 +0800

    update docstring.

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
             [org.corfield.build :as bb]))
 
 (def lib 'net.clojars.caesarhu/taiwan-id)
-(def version "0.2.3")
+(def version "0.2.4")
 #_ ; alternatively, use MAJOR.MINOR.COMMITS:
 (def version (format "1.0.%s" (b/git-count-revs nil)))
 
